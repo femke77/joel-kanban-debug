0487a8fac2e8e68a597a128b5e9e525ddf5cf260
femke77 <megan.meyers.388@gmail.com>
2024-10-23 14:24:36 -0700

    init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 const forceDatabaseRefresh = false;
-// import { seedAll } from './seeds/index.js';
+import { seedAll } from './seeds/index.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -18,12 +18,12 @@ app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
-// await seedAll();
 if (process.env.NODE_ENV === "production") {
   app.get("*", (_req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
+await seedAll();
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
